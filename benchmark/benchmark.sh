e4cf94a0f76facefc192d86f026df18298196ca6
Julia Kardasz <juliakardasz.jk@gmail.com>
2024-05-23 19:01:24 +0200

    generating data added

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -1,30 +1,30 @@
 #!bin/bash
 
 types=("--hamilton" "--nonhamilton")
+actions=("FindEuler" "FindHamilton")
 
 benchmark() {
-    runcpp="../src/a.out"
+    runcpp="../src/a.out $type"
     name_result="$type.csv"
     echo "Benchmarking Algorithm $type with input $input_file"
     result=$(${runcpp} $type < $input_file) 
-    #pierwsza linijka to czy jest czy nie ma i druga to czas, musze pomyslec jaki bedzie output dokladnie
-    if [[ $($type | cut -c 1-2) == "hamilton" ]]
-    then
-        
-    else
-
-    fi
-    size=$(cat $input_file | head -n -1)
+    
     echo "$type, $size, $time" >> $name_result
+    result=$(/usr/bin/time -f "%S" $run_project < <(cat $input_file $action <(echo Exit)) 2>&1 >$tmpFile)
+    time=${result%|*}
+
+    echo "$instance_size,$time" >> $action_benchmark_time.csv
 }
 
 #jeszcze dodac wszystkie z tego dane pliki, taka tablice zrobic ale idk jak te dane beda wygladac
 
 echo "Algorithm, InputSize, Time" > benchmark_results.csv
 
-for file in "${input_files[@]}"; do
+for file in "data/generate."*; do
     for type in "${types[@]}"; do
-        benchmark $input_file $type
+        for action in "$(actions[@])"; do
+            benchmark $input_file $type $action
+        done
     done
 done
 
---
+++ benchmark/generate.py
@@ -0,0 +1,48 @@
+import os
+import shutil
+import sys
+
+graph_types = ["hamilton", "nonhamilton"]
+
+def generate_file(filename, n, saturation, type):
+    with open(filename, 'w') as file:
+        if(type == "hamilton"):
+            file.write(f"{n}\n{saturation}\n")
+        elif(type == "nonhamilton"):
+            file.write(f"{n}\n")
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 3:
+        print("Usage: python generate.py <start_exponent> <end_exponent>")
+        sys.exit(1)
+    try:
+        start_exponent = int(sys.argv[1])
+        end_exponent = int(sys.argv[2])
+    except ValueError as e:
+        print(f"Error: {e}")
+        print("Usage: python generate.py <start_exponent> <end_exponent>")
+        sys.exit(1)
+
+    # Directory where files will be saved
+    directory = 'data'
+
+    # Clear the contents of the benchmark directory
+    if os.path.exists(directory):
+        shutil.rmtree(directory)
+    os.makedirs(directory)
+
+    max_n_length = len(str(2 ** end_exponent))  # Determine the length of the largest n
+    for t in graph_types:
+        for i in range(start_exponent, end_exponent + 1):
+            if(t == "hamilton"):
+                saturation = 30
+                filename = os.path.join(directory, f"{t}{saturation}_generate{i}.txt")
+                generate_file(filename, i, 30,t)
+                saturation = 70
+                filename = os.path.join(directory, f"{t}{saturation}_generate{i}.txt")
+                generate_file(filename, i, 70, t)
+            elif(t == "nonhamilton"):
+                saturation = 50
+                filename = os.path.join(directory, f"{t}_generate{i}.txt")
+                generate_file(filename, i, 50, t)
